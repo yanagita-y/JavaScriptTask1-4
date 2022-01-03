d9734d541e3c6d9d75a0d99139e3ad7adfc8236e
DESKTOP-6LCURLL\Yanagita <yosiphi@hotmail.com>
2022-01-04 00:42:52 +0900

    2022/1/4 JavaScript課題1-4提出

---
+++ task.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const title='======================\n現在持っているタスク一覧\n======================';
+const tasklist=[`[内容]机を片付ける、[ジャンル]掃除`,`[内容]牛乳を買う、[ジャンル]買い物`,`[内容]散歩する、[ジャンル]運動`];
+
+const out = () => {
+    console.log(title);
+    tasklist.forEach((task, index) => console.log(`${index} : ${task}`));
+}
+
+const task = () => {
+    let task = prompt('タスクを入力してください。');
+    let genre = prompt('ジャンルを入力してください。');
+    tasklist.push(`[内容]${task}` + `[ジャンル]${genre}`);
+    alert('新しいタスクを追加しました。');
+    out();
+    prompt("「確認,追加,削除,終了」の４つのいずれかを入力してください");
+    task();
+}
