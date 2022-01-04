ac5e62322f10ff961a6a6244d38f7879441a92fd
DESKTOP-6LCURLL\Yanagita <yosiphi@hotmail.com>
2022-01-04 19:03:10 +0900

    2022/1/4 JavaScriptTask①-4修正

--- task.js
+++ task.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const title='======================\n現在持っているタスク一覧\n======================';
-const tasklist=[`[内容]机を片付ける、[ジャンル]掃除`,`[内容]牛乳を買う、[ジャンル]買い物`,`[内容]散歩する、[ジャンル]運動`];
+const title = '======================\n現在持っているタスク一覧\n======================';
+const tasklist = [`[内容]机を片付ける、[ジャンル]掃除`,`[内容]牛乳を買う、[ジャンル]買い物`,`[内容]散歩する、[ジャンル]運動`];
 
-const out = () => {
+const showTasks = () => {
     console.log(title);
     tasklist.forEach((task, index) => console.log(`${index} : ${task}`));
 }
@@ -13,7 +13,7 @@ const task = () => {
     let genre = prompt('ジャンルを入力してください。');
     tasklist.push(`[内容]${task}` + `[ジャンル]${genre}`);
     alert('新しいタスクを追加しました。');
-    out();
+    showTasks();
     prompt("「確認,追加,削除,終了」の４つのいずれかを入力してください");
     task();
 }
